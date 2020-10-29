22b63f6eaf1d52e023037479ca60a61b046728f2
Dhiraj <dkhapangi13@outlook.com>
2020-10-29 10:22:07 +0545

    Connecting To MongoDB

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 const express = require("express")
-const app = express()
+const mongoose = require("mongoose")
 require("dotenv").config()
+
+// app
+const app = express()
+
+// db
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+}).then(() => console.log("DB Connected"))
 
 app.get("/", (req, res) => {
     res.send("hello from node updated")
