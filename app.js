8a3db5dba6488c140d7cf2203ac883e4ef85bb03
Dhiraj <dkhapangi13@outlook.com>
2020-10-30 10:51:37 +0545

    User Signin Using JWT

--- app.js
+++ app.js
@@ -1,34 +1,36 @@
-const express = require("express")
-const mongoose = require("mongoose")
-const morgan = require("morgan")
-const bodyParser = require("body-parser")
-const cookieParser = require("cookie-parser")
-const expressValidator = require('express-validator')
-require("dotenv").config()
+const express = require("express");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const expressValidator = require("express-validator");
+require("dotenv").config();
 // import routes
-const userRoutes = require("./routes/user")
+const userRoutes = require("./routes/user");
 
 // app
-const app = express()
+const app = express();
 
 // db
-mongoose.connect(process.env.MONGO_URI, {
+mongoose
+  .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useCreateIndex: true,
-    useUnifiedTopology: true
-}).then(() => console.log("DB Connected"))
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("DB Connected"));
 
 // middlewares
-app.use(morgan('dev'))
-app.use(bodyParser.json())
-app.use(cookieParser())
-app.use(expressValidator())
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(expressValidator());
 
 // routes middleware
-app.use("/api", userRoutes)
+app.use("/api", userRoutes);
 
-const port = process.env.PORT || 8000
+const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`)
-})
+  console.log(`Server is running on port ${port}`);
+});
