3327b3a592a0de2aff80f4c038f90a04424e7f20
Dhiraj <dkhapangi13@outlook.com>
2020-10-31 09:28:45 +0545

    Creating Category Using Postman

--- routes/category.js
+++ routes/category.js
@@ -2,7 +2,12 @@ const express = require("express");
 const router = express.Router();
 
 const { create } = require("../controllers/category");
+const { requireSignin, isAuth, isAdmin } = require("../controllers/auth");
 
-router.post("/category/create", create);
+const { userById } = require("../controllers/user");
+
+router.post("/category/create/:userId", requireSignin, isAuth, isAdmin, create);
+
+router.param("userId", userById);
 
 module.exports = router;
